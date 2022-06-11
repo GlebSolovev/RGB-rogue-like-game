a6bc4b46523a5bc0643223c9fe21a1ec44fa8f39
dlochmelis33 <dlochmelis@gmail.com>
2022-06-12 02:29:46 +0300

    Add FightEngine comments

--- src/main/kotlin/ru/hse/sd/rgb/gamelogic/engines/fight/FightEngine.kt
+++ src/main/kotlin/ru/hse/sd/rgb/gamelogic/engines/fight/FightEngine.kt
@@ -3,6 +3,7 @@ package ru.hse.sd.rgb.gamelogic.engines.fight
 import ru.hse.sd.rgb.gamelogic.entities.GameUnit
 import ru.hse.sd.rgb.gamelogic.entities.GameUnitId
 import ru.hse.sd.rgb.gamelogic.entities.HpGameUnit
+import ru.hse.sd.rgb.gamelogic.entities.GameEntity
 import ru.hse.sd.rgb.utils.messaging.Ticker
 import ru.hse.sd.rgb.utils.messaging.messages.ColorTick
 import ru.hse.sd.rgb.utils.messaging.messages.HpChanged
@@ -34,6 +35,25 @@ data class BaseColorStats(
 )
 
 @Suppress("TooManyFunctions")
+/**
+ * Class for processing color-based operations such as attacks and updates.
+ *
+ * The specific rules for color interactions are defined by:
+ * - so-called base colors, which specify which attack the colors can perform
+ * - interaction matrix, which defines how much damage one color deals to another
+ * upon attacking.
+ *
+ * During the game, actual [RGB] colors are mapped to the base color closest by
+ * some internal metric (currently L1). The damage defined by interaction matrix
+ * is also multiplied by a coefficient dependent on the distance between the base
+ * color and the actual color.
+ *
+ * As the only class that is allowed to perform color updates, this class also
+ * stores [Ticker]s that govern units' color update periods.
+ *
+ * @param baseColors The list of all base colors and their properties.
+ * @param interactionMatrix The matrix that defines damage from one color to another.
+ */
 class FightEngine(
     baseColors: List<BaseColorStats>,
     interactionMatrix: Grid2D<Int>
@@ -51,6 +71,10 @@ class FightEngine(
     private val unitColorTickers = ConcurrentHashMap<GameUnitId, Ticker>() // TODO: not concurrent ?
     private val unsafeMethods: UnsafeMethodsImpl = UnsafeMethodsImpl()
 
+    /**
+     * Methods that must be used instead of their usual FightEngine counterparts if
+     * and only if explicitly told to.
+     */
     interface UnsafeMethods {
         fun unsafeChangeRGB(unit: GameUnit, newRgb: RGB)
         fun unsafeAttack(from: GameUnit, to: GameUnit)
@@ -131,6 +155,16 @@ class FightEngine(
         return (attackFromTo[from.cachedBaseColorId to to.cachedBaseColorId]!! * similarityCoefficient).roundToInt()
     }
 
+    /**
+     * Registers [unit] in this FightEngine.
+     *
+     * This includes starting a color [Ticker] for [unit].
+     *
+     * Any FightEngine operations involving unregistered units lead to
+     * undefined behaviour (most likely, a [NullPointerException]).
+     *
+     * @param unit The unit to register.
+     */
     suspend fun registerUnit(unit: GameUnit) {
         val mutex = Mutex()
         mutex.withLock {
@@ -139,6 +173,15 @@ class FightEngine(
         }
     }
 
+    /**
+     * Unregisters [unit] from this FightEngine.
+     *
+     * This includes stopping the [Ticker] for [unit].
+     *
+     * The main purpose of this method is to avoid memory leaks.
+     *
+     * @param unit The unit to unregister.
+     */
     suspend fun unregisterUnit(unit: GameUnit) {
         val mutex = unitMutexes[unit.id]!!
         mutex.withLock {
@@ -151,37 +194,103 @@ class FightEngine(
     private fun resolveBaseColor(rgb: RGB): BaseColorId =
         baseColorStats.minWithOrNull(Comparator.comparing { it.value.rgb l1Norm rgb })!!.key
 
+    /**
+     * Unconditionally sets the color of [unit] to [newRgb].
+     *
+     * @param unit The unit to change the color of.
+     * @param newRgb The new [RGB] value.
+     */
     suspend fun changeRGB(unit: GameUnit, newRgb: RGB) {
         withLockedUnits(setOf(unit)) {
             unsafeMethods.unsafeChangeRGB(unit, newRgb)
         }
     }
 
+    /**
+     * Unconditionally adds [delta] to [unit]'s color.
+     *
+     * @param unit The unit to change the color of.
+     * @param delta The [RGBDelta] to add to [unit]'s color.
+     */
     suspend fun changeRGB(unit: GameUnit, delta: RGBDelta) {
         withLockedUnits(setOf(unit)) {
             unsafeMethods.unsafeChangeRGB(unit, unit.gameColor + delta)
         }
     }
 
+    /**
+     * Calculates and performs an attack from one [GameUnit] to another.
+     *
+     * This method involves three steps:
+     *  - calculating attack parameters based on [from]'s color
+     *  - changing hp of [to] accordingly
+     *  - sending a [HpChanged] message to [to]
+     *
+     *  If [from] and [to] parent entities have the same [GameEntity.FightEntity.teamId],
+     *  no attack is performed.
+     *
+     *  Note: healing is considered a special kind of attack, where the damage
+     *  value is negative.
+     *
+     *  @param from The unit that attacks [to].
+     *  @param to The unit being attacked by [from].
+     */
     suspend fun attack(from: GameUnit, to: GameUnit) {
         withLockedUnits(setOf(from, to)) {
             unsafeMethods.unsafeAttack(from, to)
         }
     }
 
-    // attack can be negative = heal
+    /**
+     *  Performs an attack on [to] with [attack] damage.
+     *
+     *  Instead of [FightEngine.attack], this method doesn't have a source of
+     *  an attack, and the [attack] value is not influenced by any base colors.
+     *  A [HpChanged] message is still sent to [to] normally.
+     *
+     *  This method can be called by items, for instance, since they don't have
+     *  any [GameUnit]s.
+     *
+     *  Note: healing is considered a special kind of attack, where the damage
+     *  value is negative.
+     *
+     * @param to The unit being attacked.
+     * @param attack The damage to be applied to [to].
+     */
     suspend fun attackDirectly(to: GameUnit, attack: Int) {
         withLockedUnits(setOf(to)) {
             unsafeMethods.unsafeAttackDirectly(to, attack)
         }
     }
 
+    /**
+     * Unconditionally adds [amount] to [unit]'s maxHp property.
+     *
+     * @param unit The unit being changed.
+     * @param amount The delta that is added to [unit]'s maxHp.
+     */
     suspend fun changeMaxHp(unit: HpGameUnit, amount: Int) {
         withLockedUnits(setOf(unit)) {
             unit.maxHp += amount
         }
     }
 
+    /**
+     * Performs a color update for [unit].
+     *
+     * A color update means activating all of [BaseColorUpdateEffect]s that are
+     * defined by the closest base color.
+     *
+     * While [unit]'s color cannot be changed during this method from a different thread,
+     * one of the activated effects might (drastically) change it. To avoid impending
+     * nonsense (like green healing fireballs), not all effects may get activated.
+     *
+     * This method will do nothing if unit's parent [GameEntity.FightEntity.isUnitActive]
+     * for [unit] returns false.
+     *
+     * @param unit The unit being updated.
+     * @param controlParams The parameters for controlling activated effects.
+     */
     suspend fun update(unit: GameUnit, controlParams: ControlParams) {
         if (!unit.parent.fightEntity.isUnitActive(unit)) return
         withLockedUnits(setOf(unit)) {
@@ -197,6 +306,11 @@ class FightEngine(
         }
     }
 
+    /**
+     * Returns the current base color name for [unit].
+     *
+     * @param unit Self-explanatory.
+     */
     fun getBaseColorName(unit: GameUnit): String = getBaseColorStats(unit).name
 
     private fun getBaseColorStats(unit: GameUnit): BaseColorStats = unit.cachedBaseColorId.stats
@@ -209,5 +323,8 @@ class FightEngine(
 
     private val teamGenerator = AtomicInteger(0)
 
+    /**
+     * Returns a unique teamId.
+     */
     fun newTeamId() = teamGenerator.incrementAndGet()
 }
